93cabb62195f4971de87ca9d6cf2787455bd1e9a
shubhamishra4933-dot <shubhamishra4933@gmail.com>
2026-06-18 13:43:57 +0530

    Add @Component annotation to JwtUtil class

--- security/JwtUtil.java
+++ security/JwtUtil.java
@@ -1,7 +1,10 @@
 package com.shubham.jwtauth.security;
 
+import org.springframework.stereotype.Component;
+
 import java.util.Date;
 
+@Component
 public class JwtUtil {
 
     public String generateToken(String username) {
