82ffd957f7d9b5d37530d1682895288a193d5dd3
shubhamishra4933-dot <shubhamishra4933@gmail.com>
2026-06-18 13:31:53 +0530

    Initialize JwtAuthApplication with Spring Boot

--- src/main/java/com/shubham/jwtauth/JwtAuthApplication.java
+++ src/main/java/com/shubham/jwtauth/JwtAuthApplication.java
@@ -1,1 +1,16 @@
 
+package com.shubham.jwtauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(
+                JwtAuthApplication.class,
+                args
+        );
+    }
+}
